7fabb8da3b5ffe7f05aa732e2e0849d1d70f39c2
Cameron Bennett <cameron.bennett.91@hotmail.com>
2013-04-23 18:48:29 +1000

    ElementList (XML read) Upload
    Self explanitory

---
+++ ElementList.cs
@@ -0,0 +1,25 @@
+﻿#region Include libraries
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+#endregion
+
+namespace Platformer
+{
+    public class ElementList
+    {
+        public List<ElementDefinition> Elements;
+
+        public ElementList loadDefinitions()
+        {
+            var fileStream = new FileStream("Content/ElementList.xml", FileMode.Open);
+            var xmlSerializer = new XmlSerializer(typeof(ElementList));
+            ElementList definitions = (ElementList)xmlSerializer.Deserialize(fileStream);
+            return definitions;
+        }
+    }// End of ElementList Class
+}
